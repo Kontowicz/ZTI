4db4bcca80a3ca48d079d44688aab321253bbad6
Mikolaj-Walkowiak <mwalkowiak97@gmail.com>
2020-01-20 15:07:24 +0100

    smaller dict but also smaller gt

--- dicts.py
+++ dicts.py
@@ -2,12 +2,12 @@ big_dict = {
      ('Organisation', 'Organisation'): ['affiliation'],
      ('Person', 'EducationalInstitution'): ['almaMater'],
      ('Band', 'Person'): ['formerBandMember', 'bandMember'],
-     ('Person', 'Place'): ['deathPlace', 'birthPlace'],
+     ('Person', 'Place'): ['deathPlace', 'birthPlace', 'location'],
      ('Organisation', 'Person'): ['president', 'ceo'],
      ('Person', 'Person'): ['spouse', 'relative', 'parent', 'child'],
      ('Athlete', 'SportsTeam'): ['formerTeam', 'debutTeam', 'club'],
      ('Organisation', 'Country'): ['country'],
-     ('Person', 'Country'): ['country'],
+     ('Person', 'Country'): ['country','nationality'],
      ('Place', 'Country'): ['country'],
      ('PopulatedPlace', 'PopulatedPlace'): ['department'],
      ('Place', 'PopulatedPlace'): ['district'],
@@ -18,11 +18,8 @@ big_dict = {
      ('Organisation', 'Settlement'): ['hometown'],
      ('Person', 'Settlement'): ['hometown'],
      ('PopulatedPlace', 'Person'): ['leaderName'],
-     ('Place', 'Place'): ['locatedInArea'],
+     ('Place', 'Place'): ['location','locatedInArea'],
      ('Organisation', 'Place'): ['location'],
-     ('Person', 'Place'): ['location'],
-     ('Place', 'Place'): ['location'],
-     ('Person', 'Country'): ['nationality'],
      ('Company', 'Company'): ['subsidiary'],
      ('ArchitecturalStructure', 'Organisation'): ['tenant'],
      ('Athlete', 'Person'): ['trainer']
@@ -34,7 +31,8 @@ maps = {
     'GPE': 'Place',
     'LOC': 'PopulatedPlace',
     'FAC': 'ArchitecturalStructure',
-    'WORK_OF_ART': 'Band'
+    'WORK_OF_ART': 'Band',
+    'US' : 'UnitedStates'
 }
 
 ignore_list = [
--- test_all.py
+++ test_all.py
@@ -2,7 +2,7 @@ from glob import glob
 from parse import parse_odp as parse
 from main import own_fun
 from tqdm import tqdm
-
+from dicts import big_dict
 files = glob('training/*.ttl')
 
 def compare(pred, gt):
@@ -23,15 +23,17 @@ def compare(pred, gt):
 good = 0
 bad = 0
 total = 0
-
+possibleRelations = [item.lower() for sublist in list(big_dict.values()) for item in sublist]
 for file in tqdm(files):
     gt = parse(file)
+    gt2 = y = [g for g in gt if g[1] in possibleRelations]
+
     pred = own_fun(file)
 
-    g,b  = compare(pred, gt)
+    g,b  = compare(pred, gt2)
     good += g
     bad += b
-    total += len(gt)
+    total += len(gt2)
 
 print(f'Poprawne/wszystkie: {good}/{total}')
 print(f'Niepoprawne/wszystkie: {bad}/{total}')
